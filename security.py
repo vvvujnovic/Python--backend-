4aa6cd8ea3f80251878948197b75c4aabc0c4d14
vvujnovic <162925491+vvujnovic@users.noreply.github.com>
2024-05-22 10:08:48 +0200

    dodavanje debug poruka u securty.py datoteku da bih se uvjerili da se aut.odvija pravilno

--- security.py
+++ security.py
@@ -6,9 +6,15 @@ security = HTTPBasic()
 def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = "admin"
     correct_password = "vedro"
+    
+    print("DEBUG: Provera autentikacije...")
+    
     if credentials.username != correct_username or credentials.password != correct_password:
+        print("DEBUG: Neispravni kredencijali.")
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Neispravno korisničko ime ili lozinka",
             headers={"WWW-Authenticate": "Basic"},
         )
+    
+    print("DEBUG: Autentikacija uspešna.")
