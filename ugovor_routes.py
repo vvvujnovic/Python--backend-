4722c575fa3361871727fca7c7de5f342c56d5e5
vvujnovic <162925491+vvujnovic@users.noreply.github.com>
2024-05-27 09:41:45 +0200

    u file ugovor.py imortana kolekcija ugovora  zbog nastalog prolema kod testiranja gdje su se ugovore spremali u kolekciju usluga umjesto ugovora

--- ugovor_routes.py
+++ ugovor_routes.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import HTTPBasicCredentials
 from typing import List
-from models import Ugovor, Stavka
-from database import collection
+from models import Ugovor
+from database import ugovor_collection
 from security import authenticate
 from bson import ObjectId
 
@@ -11,8 +11,8 @@ router = APIRouter()
 @router.post("/dodaj-ugovor")
 async def dodaj_ugovor(ugovor: Ugovor, credentials: HTTPBasicCredentials = Depends(authenticate)):
     try:
-           # Spremamo zahtjev u MongoDB
-        collection.insert_one(ugovor.dict())
+        # Sprema ugovor u kolekciju Ugovor
+        ugovor_collection.insert_one(ugovor.dict())
         return {"message": "Ugovor uspješno dodan!"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -20,7 +20,7 @@ async def dodaj_ugovor(ugovor: Ugovor, credentials: HTTPBasicCredentials = Depen
 @router.get("/ugovori", response_model=List[Ugovor])
 async def get_ugovori():
     try:
-        ugovori = list(collection.find({}, {'_id': 0}))  # Ne vraćamo '_id' polje
+        ugovori = list(ugovor_collection.find({}, {'_id': 0}))  # Ne vraćamo '_id' polje
         return ugovori
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -29,7 +29,7 @@ async def get_ugovori():
 async def update_ugovor(ugovor_id: str, ugovor: Ugovor):
     try:
         updated_ugovor = {k: v for k, v in ugovor.dict().items() if v is not None}
-        result = collection.update_one({"_id": ObjectId(ugovor_id)}, {"$set": updated_ugovor})
+        result = ugovor_collection.update_one({"_id": ObjectId(ugovor_id)}, {"$set": updated_ugovor})
         if result.matched_count == 0:
             raise HTTPException(status_code=404, detail="Ugovor nije pronađen")
         return {"message": "Ugovor uspješno ažuriran"}
@@ -39,7 +39,7 @@ async def update_ugovor(ugovor_id: str, ugovor: Ugovor):
 @router.delete("/ugovori/{ugovor_id}")
 async def delete_ugovor(ugovor_id: str):
     try:
-        result = collection.delete_one({"_id": ObjectId(ugovor_id)})
+        result = ugovor_collection.delete_one({"_id": ObjectId(ugovor_id)})
         if result.deleted_count == 0:
             raise HTTPException(status_code=404, detail="Ugovor nije pronađen")
         return {"message": "Ugovor uspješno obrisan"}
